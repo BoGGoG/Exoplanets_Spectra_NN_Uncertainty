61c684657351bce451a51c21a258a8f0662a9510
Marco Knipfer <marco.knipfer@me.com>
2025-08-22 14:39:04 +0200

    Added uncertainty prediction feature

--- source/models/Model01.py
+++ source/models/Model01.py
@@ -27,6 +27,44 @@ class MeanStdNormalizer:
         return (x - mean) / (std + self.epsilon)
 
 
+class NLLLossMultivariate:
+    """
+    Negative Log Likelihood with uncertainty estimation for multivariate regression.
+    """
+
+    def __init__(self, reduction: str = "mean"):
+        self.reduction = reduction
+
+    def forward(self, y_pred, sigmas2_pred, y_true):
+        """
+        y_pred shape: [batch, n_out_features]
+        sigmas_pred shape: [batch, n_out_features], the sigma^2 predicted by the model for each variable
+        y_true shape: [batch, n_out_features]
+
+        Assuming Gaussian distribution for each output variable.
+        For each variable, we compute the NLL:
+        0.5 log(sigma^2) + 0.5 * ((y_true - mu)^2 / sigma^2)
+        """
+        assert y_pred.shape == sigmas2_pred.shape == y_true.shape
+        eps = 1e-8  # small value to avoid log(0)
+
+        losses = 0.5 * torch.log(sigmas2_pred + eps) + 0.5 * (
+            (y_true - y_pred) ** 2
+        ) / (sigmas2_pred + eps)
+        losses = losses.sum(dim=1)  # sum over output features
+        if self.reduction == "mean":
+            loss = losses.mean()
+        elif self.reduction == "sum":
+            loss = losses.sum()
+        else:
+            loss = losses  # no reduction
+
+        return loss
+
+    def __call__(self, y_pred, sigmas2_pred, y_true):
+        return self.forward(y_pred, sigmas2_pred, y_true)
+
+
 class Model_01(nn.Module):
     """
     Multivariate regression model:
@@ -49,13 +87,44 @@ class Model_01(nn.Module):
             self.fc.add_module(f"fc_{i}_act", nn.ReLU())
             self.fc.add_module(f"fc_{i}_dropout", nn.Dropout(0.1))
             current_dim = hidden_dim
-        self.fc.add_module(f"fc_last", nn.Linear(current_dim, self.out_features))
+        last_fc_layer_dim = current_dim
+        # self.fc.add_module(f"fc_last", nn.Linear(current_dim, self.out_features))
+
+        self.mus_model = nn.Sequential()
+        mus_model_dims = [20]
+        current_dim = last_fc_layer_dim
+        for i, hidden_dim in enumerate(mus_model_dims):
+            self.mus_model.add_module(f"mus_fc_{i}", nn.Linear(current_dim, hidden_dim))
+            self.mus_model.add_module(f"mus_fc_{i}_act", nn.ReLU())
+            self.mus_model.add_module(f"mus_fc_{i}_dropout", nn.Dropout(0.1))
+            current_dim = hidden_dim
+        self.mus_model.add_module(
+            f"mus_fc_last", nn.Linear(current_dim, self.out_features)
+        )
+
+        self.sigmas_model = nn.Sequential()
+        sigmas_model_dims = [20]
+        current_dim = last_fc_layer_dim
+        for i, hidden_dim in enumerate(sigmas_model_dims):
+            self.sigmas_model.add_module(
+                f"sigmas_fc_{i}", nn.Linear(current_dim, hidden_dim)
+            )
+            self.sigmas_model.add_module(f"sigmas_fc_{i}_act", nn.ReLU())
+            self.sigmas_model.add_module(f"sigmas_fc_{i}_dropout", nn.Dropout(0.1))
+            current_dim = hidden_dim
+        self.sigmas_model.add_module(
+            f"sigmas_fc_last", nn.Linear(current_dim, self.out_features)
+        )
+        self.softplus = nn.Softplus(beta=1.0, threshold=20.0)
 
     def forward(self, x):
         x = self.normalizer(x)
         x = self.fc(x)
+        mus = self.mus_model(x)
+        sigmas2 = self.sigmas_model(x)  # sigma^2
+        sigmas2 = self.softplus(sigmas2)  # ensure positive variance
 
-        return x
+        return {"mus": mus, "sigmas2": sigmas2}
 
 
 class Model_01_Lit(L.LightningModule):
@@ -72,7 +141,8 @@ class Model_01_Lit(L.LightningModule):
         self.train_loader = None
         self.val_loader = None
         self.test_loader = None
-        self.criterion = nn.MSELoss()
+        self.criterion = NLLLossMultivariate(reduction="mean")
+        self.pred_criterion = nn.MSELoss(reduction="mean")
         self.scaler = StandardScaler()
         self.test_step_outputs = []
         self.labels_names = None
@@ -154,7 +224,10 @@ class Model_01_Lit(L.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         out = self(x)
-        loss = self.criterion(out, y)
+        y_pred = out["mus"]
+        sigma2_pred = out["sigmas2"]
+
+        loss = self.criterion(y_pred, sigma2_pred, y)
         self.log(
             "losses/train_loss",
             loss,
@@ -168,7 +241,11 @@ class Model_01_Lit(L.LightningModule):
     def validation_step(self, batch, batch_idx):
         x, y = batch
         out = self(x)
-        loss = self.criterion(out, y)
+        y_pred = out["mus"]
+        sigma2_pred = out["sigmas2"]
+
+        loss = self.criterion(y_pred, sigma2_pred, y)
+        pred_loss = self.pred_criterion(y_pred, y)
         self.log(
             "losses/val_loss",
             loss,
@@ -177,19 +254,40 @@ class Model_01_Lit(L.LightningModule):
             prog_bar=True,
             logger=True,
         )
+        self.log(
+            "losses/val_pred_loss",
+            pred_loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+            logger=True,
+        )
         return loss
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self(x)
-        loss = self.criterion(y, y_hat)
+        out = self(x)
+        y_pred = out["mus"]
+        sigma2_pred = out["sigmas2"]
+        loss = self.criterion(y_pred, sigma2_pred, y)
+        pred_loss = self.pred_criterion(y_pred, y)
+
         self.log(
             "test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True
+        )
+        self.log(
+            "test_pred_loss",
+            pred_loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
         )
         out_dict = {
             "x": x,
             "y": y,
-            "y_hat": y_hat,
+            "y_pred": y_pred,
+            "sigmas2_pred": sigma2_pred,
         }
         self.test_step_outputs.append(out_dict)
         return loss
@@ -197,11 +295,14 @@ class Model_01_Lit(L.LightningModule):
     def on_test_epoch_end(self):
         x = torch.cat([x["x"] for x in self.test_step_outputs], dim=0)
         y = torch.cat([x["y"] for x in self.test_step_outputs], dim=0)
-        y_hat = torch.cat([x["y_hat"] for x in self.test_step_outputs], dim=0)
+        y_pred = torch.cat([x["y_pred"] for x in self.test_step_outputs], dim=0)
+        sigmas2_pred = torch.cat(
+            [x["sigmas2_pred"] for x in self.test_step_outputs], dim=0
+        )
 
         figs = []
         loss_f = torch.nn.MSELoss(reduction="none")
-        indiv_losses_per_var = loss_f(y, y_hat)
+        indiv_losses_per_var = loss_f(y, y_pred)
         indiv_losses = indiv_losses_per_var.mean(dim=1).detach().cpu().numpy()
         indiv_losses_per_var = indiv_losses_per_var.detach().cpu().numpy()
 
@@ -230,17 +331,30 @@ class Model_01_Lit(L.LightningModule):
         plt.tight_layout()
         self.logger.experiment.add_figure("indiv_losses_per_var_hist", fig)
 
-        # self.logger.experiment.add_figure("peaks", figs)
-
     def configure_optimizers(self):
         """ToDo: Scheduler"""
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
 
+def test_NLLLoss():
+    # test NLL loss
+    criterion_indiv = NLLLossMultivariate(reduction=None)
+    criterion = NLLLossMultivariate(reduction="mean")
+    y_pred = torch.tensor([[1.0, 2.0], [3.0, 4.0]], dtype=torch.float32)
+    y_true = torch.tensor([[1.0, 2.5], [3.1, 3.0]], dtype=torch.float32)
+    sigmas2_pred = torch.tensor([[0.1, 0.2], [0.3, 0.4]], dtype=torch.float32)
+
+    loss = criterion_indiv(y_pred, sigmas2_pred, y_true)
+    print("Loss without reduction:", loss)
+
+    loss_mean = criterion.forward(y_pred, sigmas2_pred, y_true)
+    print("Loss with mean reduction:", loss_mean)
+
+
 if __name__ == "__main__":
     data_dir = Path("data") / "cleaned_up_version"
-    n_load = 1_000
+    n_load = 5_000
 
     hparams = {
         "data_dir": data_dir / "train_test_split",
@@ -257,7 +371,7 @@ if __name__ == "__main__":
     )
     model = Model_01_Lit(hparams)
     trainer = L.Trainer(
-        max_epochs=5,
+        max_epochs=10,
         accelerator="auto",
         devices=1,
         logger=logger,
